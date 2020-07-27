b0ba463690a196a685dc8254bd47f8ccf58ead6e
Ahmad <antho.firuze@gmail.com>
2020-07-27 11:49:28 +0700

    Flutter Lesson 2

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterapp/app/locator.dart';
 import 'package:flutterapp/app/router.gr.dart';
 import 'package:flutterapp/ui/views/home/home_view.dart';
+import 'package:flutterapp/ui/views/partial_builds/partial_builds_view.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 void main() {
@@ -14,7 +15,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      initialRoute: Routes.startupView,
+      // initialRoute: Routes.startupView,
+      // Testing only
+      home: PartialBuildsView(),
       onGenerateRoute: Router().onGenerateRoute,
       navigatorKey: locator<NavigationService>().navigatorKey,
     );
